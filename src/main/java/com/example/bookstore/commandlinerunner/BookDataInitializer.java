e501beaa40161b6a478dc51d760e907ad2c30268
Reinis Kālbergs <r.kalbergs@gmail.com>
2023-09-09 14:42:10 +0300

    Fixed sql script for postgresql and fixed fake data generation in case it has benn done

--- src/main/java/com/example/bookstore/commandlinerunner/BookDataInitializer.java
+++ src/main/java/com/example/bookstore/commandlinerunner/BookDataInitializer.java
@@ -1,26 +1,41 @@
 package com.example.bookstore.commandlinerunner;
 
 import com.example.bookstore.dto.BookCreateRequest;
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.repository.BookRepository;
 import com.example.bookstore.service.BookService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class BookDataInitializer implements CommandLineRunner {
 
     private final BookService bookService;
+    private final BookRepository bookRepository;
     @Value("${app.initializeBooks:false}")
     private boolean initializeBooks;
 
     @Override
     public void run(String... args) {
         if (initializeBooks) {
-            for (int i = 1; i <= 100; i++) {
-                BookCreateRequest request = new BookCreateRequest("Book " + i, "Author " + i);
-                bookService.createBook(request);
+            Optional<Book> firstBook = bookRepository.findByName("Book 1");
+
+            if (firstBook.isPresent()) {
+                log.info("Fake data has already been generated");
+            } else {
+                log.info("Generating 100 fake books");
+                for (int i = 1; i <= 100; i++) {
+                    BookCreateRequest request = new BookCreateRequest("Book " + i, "Author " + i);
+                    bookService.createBook(request);
+                }
+                log.info("Fake data generation is complete");
             }
         }
     }
